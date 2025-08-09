ef6f8e0c9f1a8c52c83c019f8c8e628d5f736ff8
miguel tostado <mtalamo2000@gmail.com>
2025-08-09 09:25:06 -0600

    tests and progress in finding the best pin. Not working properly

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -15,7 +15,9 @@ SIZE = (WIDTH, HEIGHT)
 
 # GRAB IMAGE & CONVERT TO GRAYSCALE
 img = Image.open('images/imgTest1.png').convert('L')
-img2 = np.asarray(img.resize(SIZE, Image.LANCZOS))
+img2 = img.resize(SIZE, Image.LANCZOS)
+
+imgArr = np.asarray(img2)
 
 # imgplot = plt.imshow(img2, cmap="gray")
 
@@ -23,7 +25,6 @@ img2 = np.asarray(img.resize(SIZE, Image.LANCZOS))
 # SET PINS
 def setPins(nailCount, size):
     arrPins = []
-    center = (size[0] // 2, size[1] // 2)
     radius = size[0] // 2 - 10
     # print(center)
     # print(radius)
@@ -35,13 +36,34 @@ def setPins(nailCount, size):
     return arrPins
 
 arrPins = setPins(NUM_PINS, SIZE)
-print(arrPins)
+# print(arrPins)
 
 # GREEDY ALGORITHM
-def exploreAllPaths():
+def exploreAllPaths(arrPins, originalImg, NUM_PINS, NUM_LINES):
+    currentPin = (arrPins[0][0], arrPins[0][1])
+    currentBest = -9999
+    for i in range(200):
+        for x, y in arrPins:
+            if currentPin == (x, y):
+                continue
+
+            # Determine best next pin
+            tempImg = originalImg.copy()
+            draw = ImageDraw.Draw(tempImg)
+            draw.line([(currentPin[0], currentPin[1]), (x, y)], fill='black', width=THICKNESS)
+            result = np.asarray(originalImg) - np.array(tempImg)
+            result = np.sum(result)
+            if result > currentBest:
+                currentBest = result
+                nextPin = (x, y)
+        print("current", currentPin)
+        print("next", nextPin)
+        draw = ImageDraw.Draw(originalImg)
+        draw.line([(currentPin[0], currentPin[1]), (x, y)], fill='black', width=THICKNESS)
+        currentPin = nextPin
     return
 
-exploreAllPaths()
+exploreAllPaths(arrPins, img2, NUM_PINS, NUM_LINES)
 
 # Show
 for items in arrPins:
@@ -51,8 +73,15 @@ for items in arrPins:
 x= [arrPins[0][0], arrPins[50][0]]
 y = [arrPins[0][1], arrPins[50][1]]
 
-plt.plot(x,y, color='black')
-imgplot = plt.imshow(img2, cmap="gray")
+img3 = img2.copy()
+draw = ImageDraw.Draw(img3)
+draw.line([(0, 0), (250,10)], fill="black", width=THICKNESS)
+diff = imgArr - np.array(img3)
+print(np.sum(diff))
+imgArr = np.asarray(img2)
+
+# plt.plot(x,y, color='black')
+imgplot = plt.imshow(imgArr, cmap="gray")
 
 plt.colorbar()
 plt.show()
