b5dfb1e5056683e1ef178f2a01c7b205eb8d84f3
miguel tostado <mtalamo2000@gmail.com>
2025-08-10 21:34:02 -0600

    Program now functional, needs improvement on readability

--- Readme.md
+++ Readme.md
@@ -17,3 +17,16 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 mathplotlib
 pillow
 numpy
+
+
+# ALL REFERENCES USED
+
+## STRING ART
+https://www.youtube.com/watch?v=qH7ZgQghKUU
+https://www.youtube.com/watch?v=UsbBSttaJos
+https://www.youtube.com/watch?v=1RQMCCSR0HA&t
+https://www.youtube.com/watch?v=WGccIFf6MF8
+
+
+## BRESENHAM LINE
+https://saturncloud.io/blog/bresenham-line-algorithm-a-powerful-tool-for-efficient-line-drawing/
--- index.py
+++ index.py
@@ -5,25 +5,24 @@ import cv2
 import math
 
 # MAIN 
-NUM_PINS = 200
+NUM_PINS = 250
 NUM_LINES = 2000
 THICKNESS = 1
-WIDTH = 300
-HEIGHT = 300
+WIDTH = 800
+HEIGHT = 800
 SIZE = (WIDTH, HEIGHT)
 
 
 # GRAB IMAGE & CONVERT TO GRAYSCALE
-img = Image.open('images/imgTest1.png').convert('L')
+img = Image.open('images/test3.jpeg').convert('L')
 img2 = img.resize(SIZE, Image.LANCZOS)
-imgArr = np.asarray(img2).astype(np.int16)
-
+# NORMALIZE ARRAY (0, 1)
+imgArr = np.asarray(img2, dtype=np.float32) / 255
 
 # SET PINS
 def setPins(nailCount, size):
     arrPins = []
     radius = size[0] // 2 - 10
-    # print(radius)
     for i in range(nailCount):
         angle = (2 * math.pi * i) / nailCount
         x = size[0] // 2 + radius * math.cos(angle)
@@ -34,6 +33,63 @@ def setPins(nailCount, size):
 arrPins = setPins(NUM_PINS, SIZE)
 # print(arrPins)
 
+def bresenham_line(nail1, nail2):
+
+    x1, y1 = nail1
+    x2, y2 = nail2
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    slope = dy > dx
+
+    if slope:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    error = dx // 2
+    y = y1
+    ystep = 1 if y1 < y2 else -1
+
+    pixels = []
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if slope else (x, y)
+        pixels.append(coord)
+        error -= dy
+        if error < 0:
+            y += ystep
+            error += dx
+
+    return pixels
+
+def calculateScore(nail1, nail2, workingImg):
+
+    pixels = bresenham_line(nail1, nail2)
+
+    if not pixels:
+        return 0.0
+
+    totalContrast = 0
+    for x, y in pixels:
+        pixelIndex = workingImg[y, x]
+        darkness = 1.0 - pixelIndex
+        totalContrast += darkness
+
+    return totalContrast / len(pixels)
+
+def updateImage(nail1, nail2, workingImg):
+    pixels = bresenham_line(nail1, nail2)
+    bright = 0.5
+    for x, y in pixels:
+        pixelIndex = workingImg[y,x]
+        newVal = min(1.0, pixelIndex + bright)
+        workingImg[y,x] = newVal
+    return workingImg
+
 # GREEDY ALGORITHM
 def exploreAllPaths(arrPins, originalImg, NUM_LINES):
 
@@ -41,9 +97,10 @@ def exploreAllPaths(arrPins, originalImg, NUM_LINES):
     arrNails = [currentNail]
     solutionBoard = Image.new('L', [WIDTH, HEIGHT], 255)
     draw = ImageDraw.Draw(solutionBoard)
-    visited_lines = set()
+    workingImg = originalImg.copy()
 
     for _ in range(NUM_LINES):
+
         nextNail = None
         highestContrast = -1
 
@@ -52,77 +109,34 @@ def exploreAllPaths(arrPins, originalImg, NUM_LINES):
             if nail == currentNail:
                 continue
 
-            if len(arrNails) > 1 and nail == arrNails[-2]:
-                continue
-
-            line_key = tuple(sorted([currentNail, nail]))
-            if line_key in visited_lines:
-                continue
-
-
-            steps = 100
-            totalContrast = 0
-            for i in range(steps):
-                x = int(math.floor(currentNail[0] + (nail[0] - currentNail[0]) * (i/steps)))
-                y = int(math.floor(currentNail[1] + (nail[1] - currentNail[1]) * (i/steps)))
-
-                if x >= 0 and x < WIDTH and y >= 0 and y < HEIGHT:
-                    brightness = int(originalImg[y, x])
-                    totalContrast += (255 - brightness)
-
-            contrast = totalContrast / steps
+            contrast = calculateScore(currentNail, nail, workingImg)
 
             if contrast > highestContrast:
                 highestContrast = contrast
                 nextNail = nail
-            #Get the contrast
-
 
         if nextNail == None:
             break
 
         arrNails.append(nextNail)
         #Draw the next nail
-        draw.line([currentNail, nextNail], fill="black", width=THICKNESS)
-        visited_lines.add(tuple(sorted([currentNail, nextNail])))
-        updateImage(currentNail, nextNail, originalImg, steps)
+        draw.line([currentNail, nextNail], fill=0, width=THICKNESS)
+
+        updateImage(currentNail, nextNail, workingImg)
         currentNail = nextNail
 
-    return arrNails, solutionBoard
+    return arrNails, solutionBoard, workingImg
 
-def updateImage(nail1, nail2, originalImg, steps):
-    for i in range(steps):
-        x = int(math.floor(nail1[0] + (nail2[0] - nail1[0]) * (i/steps)))
-        y = int(math.floor(nail1[1] + (nail2[1] - nail1[1]) * (i/steps)))
-        if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-            originalImg[y, x] = max(0, originalImg[y, x] - 10)
-    return originalImg
-
-
-arrNails, solutionBoard = exploreAllPaths(arrPins, imgArr, NUM_LINES)
-print(arrNails)
+arrNails, solutionBoard, workingImg = exploreAllPaths(arrPins, imgArr, NUM_LINES)
 
 # Show circumference
 for items in arrPins:
     plt.scatter(items[0], items[1], marker="o", s=THICKNESS)
 
-#TEST LINES
-# x= [arrPins[0][0], arrPins[50][0]]
-# y = [arrPins[0][1], arrPins[50][1]]
 
-# img3 = img2.copy()
-# draw = ImageDraw.Draw(img3)
-# draw.line([(0, 0), (250,10)], fill="black", width=THICKNESS)
-# diff = imgArr - np.array(img3)
-# print(np.sum(diff))
-# imgArr = np.asarray(img2)
-
-# plt.plot(x,y, color='black')
 test = np.asarray(solutionBoard)
 
 imgplot = plt.imshow(solutionBoard, cmap="gray")
 
 plt.colorbar()
 plt.show()
-
-# testImg.show()
